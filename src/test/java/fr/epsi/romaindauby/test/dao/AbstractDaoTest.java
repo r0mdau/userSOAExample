076343d736346596f5f1d4dfeeaf900048d6d27b
Romain Dauby <romain.dauby@gmail.com>
2014-06-11 15:45:42 +0200

    Now using setup and teardown

--- src/test/java/fr/epsi/romaindauby/test/dao/AbstractDaoTest.java
+++ src/test/java/fr/epsi/romaindauby/test/dao/AbstractDaoTest.java
@@ -1,11 +1,13 @@
 package fr.epsi.romaindauby.test.dao;
 
+import junit.framework.TestCase;
+
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"/spring/appContext-test.xml"})
-public abstract class AbstractDaoTest {
+public abstract class AbstractDaoTest extends TestCase {
 
 }
--- src/test/java/fr/epsi/romaindauby/test/dao/UserDaoTest.java
+++ src/test/java/fr/epsi/romaindauby/test/dao/UserDaoTest.java
@@ -1,7 +1,7 @@
 package fr.epsi.romaindauby.test.dao;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.ParseException;
@@ -12,31 +12,36 @@ import fr.epsi.romaindauby.entity.User;
 public class UserDaoTest extends AbstractDaoTest {
 	@Autowired
 	UserDao userDao;
+	private Long id;
+	private String email;
 	
-	@Test
-	public void testFind() {
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
 		User user = new User();
 		user.setEmail("romain.dauby@gmail.com");
 		user.setName("dauby");
 		user.setSurname("romain");
 		user.setPassword("password");
 		userDao.save(user);
-		
-		assertTrue(userDao.find(user.getId()).getName().equals("dauby"));
-		userDao.delete(userDao.find(user.getId()));
+		this.id = user.getId();
+		this.email = user.getEmail();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		userDao.delete(userDao.find(this.id));
+	}
+	
+	@Test
+	public void testFind() {
+		assertTrue(userDao.find(this.id).getName().equals("dauby"));
 	}
 	
 	@Test
 	public void testFindByEmail() {
-		User user = new User();
-		user.setEmail("romain.dauby@gmail.com");
-		user.setName("dauby");
-		user.setSurname("romain");
-		user.setPassword("password");
-		userDao.save(user);
-		
-		assertTrue(userDao.findByEmail("romain.dauby@gmail.com").getName().equals("dauby"));
-		userDao.delete(userDao.find(user.getId()));
+		assertTrue(userDao.findByEmail(this.email).getName().equals("dauby"));
 	}
 	
 	@Test
@@ -54,18 +59,10 @@ public class UserDaoTest extends AbstractDaoTest {
 	
 	@Test
 	public void testUpdate() {
-		User user = new User();
-		user.setEmail("georges.dupont@gmail.com");
-		user.setName("Dupont");
-		user.setSurname("Georges");
-		user.setPassword("password");
-		userDao.save(user);
-		
-		User user2 = userDao.find(user.getId());
-		user2.setName("Mandi");
-		userDao.update(user2);
-		assertTrue(userDao.find(user.getId()).getName().equals("Mandi"));
-		userDao.delete(userDao.find(user.getId()));
+		User user = userDao.find(this.id);
+		user.setName("Mandi");
+		userDao.update(user);
+		assertTrue(userDao.find(this.id).getName().equals("Mandi"));
 	}
 	
 	@Test
