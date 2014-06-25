3400858ff3dea3acc5fafb3e25b59925eb895bef
Romain Dauby <romain.dauby@gmail.com>
2014-06-25 15:07:17 +0200

    HSQLDB

--- src/test/java/fr/epsi/romaindauby/test/dao/UserDaoTest.java
+++ src/test/java/fr/epsi/romaindauby/test/dao/UserDaoTest.java
@@ -1,7 +1,5 @@
 package fr.epsi.romaindauby.test.dao;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.ParseException;
@@ -12,69 +10,34 @@ import fr.epsi.romaindauby.entity.User;
 public class UserDaoTest extends AbstractDaoTest {
 	@Autowired
 	UserDao userDao;
-	private Long id;
-	private String email;
 	
-	@Before
-	public void setUp() throws Exception {
-		super.setUp();
-		User user = new User();
-		user.setEmail("romain.dauby@gmail.com");
-		user.setName("dauby");
-		user.setSurname("romain");
-		user.setPassword("password");
-		userDao.save(user);
-		this.id = user.getId();
-		this.email = user.getEmail();
+	public void testFind() {
+		assertTrue(userDao.find(1L).getName().equals("Dauby"));
 	}
 
-	@After
-	public void tearDown() throws Exception {
-		super.tearDown();
-		userDao.delete(userDao.find(this.id));
-	}
-	
 	@Test
-	public void testFind() {
-		assertTrue(userDao.find(this.id).getName().equals("dauby"));
+	public void testUpdate() {
+		User user = userDao.find(2L);
+		user.setName("TEST");
+		userDao.update(user);
+		assertTrue(userDao.find(2L).getName().equals("TEST"));
 	}
-	
-	@Test
-	public void testFindByEmail() {
-		assertTrue(userDao.findByEmail(this.email).getName().equals("dauby"));
-	}
-	
+
 	@Test
 	public void testSave() throws ParseException{
 		User user = new User();
-		user.setEmail("georges.dupont@gmail.com");
-		user.setName("Dupont");
-		user.setSurname("Georges");
-		user.setPassword("password");
+		user.setEmail("Test14@gmail.com");
+		user.setName("Test14");
+		user.setSurname("Test14");
+		user.setPassword("Test14");
 		userDao.save(user);
 
-		assertTrue(userDao.find(user.getId()).getName().equals("Dupont"));
-		userDao.delete(userDao.find(user.getId()));
+		assertTrue(userDao.find(user.getId()).getName().equals("Test14"));
 	}
-	
-	@Test
-	public void testUpdate() {
-		User user = userDao.find(this.id);
-		user.setName("Mandi");
-		userDao.update(user);
-		assertTrue(userDao.find(this.id).getName().equals("Mandi"));
-	}
-	
+
 	@Test
 	public void testDelete() {
-		User user = new User();
-		user.setEmail("georges.dupont@gmail.com");
-		user.setName("Dupont");
-		user.setSurname("Georges");
-		user.setPassword("password");
-		userDao.save(user);
-		
-		userDao.delete(userDao.find(user.getId()));
-		assertTrue(userDao.find(user.getId()) == null);
+		userDao.delete(userDao.find(3L));
+		assertTrue(userDao.find(3L) == null);
 	}
 }
